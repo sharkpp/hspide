f190480fee47b1999a9abbf1cbf18251a8df65ff
sharkpp <webmaster@sharkpp.net>
2011-07-19 13:47:46 +0000

    ディレクトリ構成変更。 ファイルオープン時の文字化け修正。 コンパイルコマンド用プロジェクト追加。
    git-svn-id: http://nasserver:888/repos/main/platform/hsp/tools/hspide@1908 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- hspide/main.cpp
+++ hspide/main.cpp
@@ -3,11 +3,15 @@
 #include <QTranslator>
 #include <QLocale>
 #include <QString>
+#include <QTextCodec>
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
 	QTranslator trans;
+
+	qApp->setApplicationName( "unifiler" );
 
 	// メッセージファイルのロード
 	trans.load(QString(":/i18n/hspide_ja.pm"));
