f2710e644f1ea2ab8956452c3d1d6f56b1cc19fc
sharkpp <webmaster@sharkpp.net>
2011-12-12 15:31:02 +0000

    関数フックのテスト
    git-svn-id: http://nasserver:888/repos/main/platform/hsp/tools/hspide@2071 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- hspdbg/hspdbg.cpp
+++ hspdbg/hspdbg.cpp
@@ -11,10 +11,17 @@
 
 extern CDbgMain* g_app;
 
+HSP3DEBUG *g_hsp3dbg = NULL;
+
+//--------------------------------------------------------------------
+// デバッグ処理デバッグウインドウ側関数
+//--------------------------------------------------------------------
+
 EXPORT BOOL WINAPI debugini(HSP3DEBUG *p1, int p2, int p3, int p4)
 {
 	//		debugini ptr  (type1)
 
+	g_hsp3dbg = p1;
 	g_app->connectToDebugger();
 	return FALSE;
 }
@@ -50,3 +57,51 @@ EXPORT BOOL WINAPI debug_notice(HSP3DEBUG *p1, int p2, int p3, int p4)
 	return FALSE;
 }
 
+//--------------------------------------------------------------------
+// デバッグ処理プラグイン側関数
+//--------------------------------------------------------------------
+
+BYTE* old_code_next = NULL;
+
+__declspec(naked)
+void hook_code_next()
+{
+	g_hsp3dbg->dbg_curinf();
+
+	char tmp[256];
+	sprintf(tmp,"hook L:%d",g_hsp3dbg->line);
+	g_app->putLog(tmp, strlen(tmp));
+
+	// 元コード呼び出し
+	_asm { jmp old_code_next }
+}
+
+EXPORT void WINAPI hspdbg_init(HSP3TYPEINFO * info)
+{
+	//		プラグイン初期化 (実行・終了処理を登録します)
+	//
+	//hsp3sdk_init( info );			// SDKの初期化(最初に行なって下さい)
+	//info->cmdfunc = cmdfunc;		// 実行関数(cmdfunc)の登録
+	//info->reffunc = reffunc;		// 参照関数(reffunc)の登録
+	//info->termfunc = termfunc;		// 終了関数(termfunc)の登録
+
+	BYTE* code_next = (BYTE*)info->hspexinfo->HspFunc_prm_next;
+
+	DWORD op;
+	// 関数フックのため一旦属性を書き込みできるようにする
+	VirtualProtect(code_next, 5, PAGE_EXECUTE_READWRITE, &op);
+
+	// 次コード読み込み関数処理前に関数を割り込ます
+	old_code_next = (BYTE*)((DWORD)code_next + *((DWORD*)(code_next+1)) + 5); // ※jmp命令があることを前提にしている...
+	*          code_next     = 0xE9; // jmp
+	*((DWORD*)(code_next+1)) = (DWORD)hook_code_next - (DWORD)code_next - 5;
+
+	// 改変検知のためページ属性を元に戻す
+	VirtualProtect(code_next, 5, op, &op);
+
+	/*
+	//	イベントコールバックを発生させるイベント種別を設定する
+    info->option = HSPEVENT_ENABLE_GETKEY;
+	info->eventfunc = eventfunc;	// イベント関数(eventfunc)の登録
+	*/
+}
