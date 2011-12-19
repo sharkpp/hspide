892f8d5b12d0b2e93ef2da2d87fea2bcadeadc32
sharkpp <webmaster@sharkpp.net>
2011-12-19 14:54:24 +0000

    メッセージ送信のテスト中
    git-svn-id: http://nasserver:888/repos/main/platform/hsp/tools/hspide@2075 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- hspdbg/dbgmain.cpp
+++ hspdbg/dbgmain.cpp
@@ -1,21 +1,39 @@
 #include "dbgmain.h"
-#include "../hspide/debuggercommand.hpp"
 
 // dummy argument for QCoreApplication
 static int argc_ = 1;
 static char *argv_[] = {""};
 
 CDbgMain::CDbgMain()
-	: QCoreApplication(argc_, argv_)
-	, m_socket(new QLocalSocket(this))
+	: QThread()
+	, m_app(NULL)
+	, m_socket(NULL)
 	, m_id(_strtoui64(getenv("hspide#attach"), NULL, 16))
 {
-	m_socket->connectToServer("test@hspide");
+//	connect(m_thread, SIGNAL(finished()), m_thread, SLOT(quit()));
+	start();
 }
 
 CDbgMain::~CDbgMain()
 {
 	m_socket->disconnectFromServer();
+	m_app->quit();
+}
+
+void CDbgMain::run()
+{
+	QCoreApplication app(argc_, argv_);
+	m_app = &app;
+
+	m_socket = new QLocalSocket(&app);
+
+	connect(m_socket, SIGNAL(connected()), this, SLOT(connected()));
+	connect(m_socket, SIGNAL(readyRead()), this, SLOT(recvCommand()));
+	connect(m_socket, SIGNAL(disconnect()), this, SLOT(deleteLater()));
+
+	m_socket->connectToServer("test@hspide");
+
+	app.exec();
 }
 
 void CDbgMain::connectToDebugger()
@@ -30,4 +48,37 @@ void CDbgMain::putLog(const char *text, int len)
 	CDebuggerCommand cmd;
 	cmd.write(m_id, 0x01, text, len);
 	m_socket->write(QByteArray((char*)cmd.data(), cmd.size()));
+}
+
+void CDbgMain::connected()
+{
+}
+
+void CDbgMain::recvCommand()
+{
+	QByteArray data = m_socket->readAll();
+	m_cmdQueue.push(data.data(), data.size());
+
+	quint64 id;
+	quint8  cmd_id;
+	quint32 length;
+
+	for(;;)
+	{
+		CDebuggerCommand::scoped_ptr ptr = m_cmdQueue.read(id, cmd_id, length);
+		if( !ptr.valid() )
+		{
+			break;
+		}
+
+		switch(cmd_id)
+		{
+		default: {
+			CDebuggerCommand cmd;
+			cmd.write(m_id, 0x02, "", 0);
+			m_socket->write(QByteArray((char*)cmd.data(), cmd.size()));
+			qDebug() <<"CDbgMain::recvCommand"<< (void*)m_socket<< id << cmd_id;
+			}
+		}
+	}
 }
--- hspdbg/dbgmain.h
+++ hspdbg/dbgmain.h
@@ -1,13 +1,19 @@
 #include <QCoreApplication>
+#include <QThread>
 #include <QLocalSocket>
+#include "../hspide/debuggercommand.hpp"
 
 class CDbgMain
-	: public QCoreApplication
+	: public QThread
 {
 	Q_OBJECT
 
+	QCoreApplication* m_app;
+
 	QLocalSocket*	m_socket;
 	long long		m_id;
+
+	CDebuggerCommand m_cmdQueue;
 
 public:
 	CDbgMain();
@@ -15,4 +21,13 @@ public:
 
 	void connectToDebugger();
 	void putLog(const char *text, int len);
+
+protected:
+
+	void run();
+
+public slots:
+
+	void connected();
+	void recvCommand();
 };
